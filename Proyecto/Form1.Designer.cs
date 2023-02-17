39dd4946199cc464d936f6af5e71a4ae0660037b
gonzalofg25 <gonzalo.francogarcia.web@adaits.es>
2023-02-17 17:02:55 +0100

    Cambio

--- Proyecto/Form1.Designer.cs
+++ Proyecto/Form1.Designer.cs
@@ -28,19 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.salida = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(24, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 151);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Botón";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // salida
+            // 
+            this.salida.BackColor = System.Drawing.SystemColors.WindowText;
+            this.salida.ForeColor = System.Drawing.SystemColors.Window;
+            this.salida.Location = new System.Drawing.Point(311, 35);
+            this.salida.Multiline = true;
+            this.salida.Name = "salida";
+            this.salida.Size = new System.Drawing.Size(380, 259);
+            this.salida.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 450);
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(713, 450);
+            this.Controls.Add(this.salida);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Programita";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox salida;
     }
 }
--- Proyecto/Form1.cs
+++ Proyecto/Form1.cs
@@ -1,3 +1,9 @@
+using System.CodeDom;
+using System.Management;
+using System.Net;
+using System.Reflection;
+
+
 namespace Proyecto
 {
     public partial class Form1 : Form
@@ -5,6 +11,98 @@ namespace Proyecto
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            salida.Text= string.Empty;
+            ObtenerUsuarioEquipo();
+            ObtenerUnidad();
+            ObeterIP();
+            ComprobarPCEnCarga();
+            ObtenerGestionMemoriaRAM();
+        }
+
+        private void ObtenerUsuarioEquipo()
+        {
+            string usuario = SystemInformation.UserName;
+            string dominio = SystemInformation.UserDomainName;
+
+            salida.Text = "Usuario: "+ usuario + Environment.NewLine
+                +"Dominio/Equipo: "+ dominio;
+        }
+
+        private void ObtenerUnidad()
+        {
+            DriveInfo[] drives = DriveInfo
+                .GetDrives()
+                .Where(disco => disco.DriveType == DriveType.Fixed)
+                .ToArray();
+            foreach (DriveInfo drive in drives)
+            {
+                double espacioLibre = drive.TotalFreeSpace;
+                double espacioTotal = drive.TotalSize;
+
+                double espacioLibrePorcentaje = (espacioLibre/ espacioTotal) * 100;
+
+                salida.Text += drive.Name + ": " + espacioLibrePorcentaje + " % "
+                    + Environment.NewLine;
+            }
+        }
+        private void ObeterIP()
+        {
+            IPAddress[] direcciones =
+                Dns.GetHostAddresses(Dns.GetHostName())
+                .Where(a => !a.IsIPv6LinkLocal)
+                .ToArray();
+            foreach(IPAddress direccion in direcciones)
+            {
+                salida.Text += Environment.NewLine + "IP: " + direccion.Address.ToString();
+            }
+        }
+        private void ComprobarPCEnCarga()
+        {
+            Type pw = typeof(PowerStatus);
+
+            PropertyInfo[]propiedades = pw.GetProperties();
+
+            object? valor = propiedades[0].GetValue(SystemInformation.PowerStatus,null);
+
+            salida.Text += valor.ToString();
+        }
+
+        private void ObtenerGestionMemoriaRAM()
+        {
+            ObjectQuery objectQuery= new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            
+            ManagementObjectSearcher managementObject = new (objectQuery);
+
+            ManagementObjectCollection collection = managementObject.Get();
+
+            foreach(ManagementObject elemento in collection)
+            {
+                decimal memoriaTotal=
+                    Math.Round(Convert.ToDecimal(elemento["TotalVisibleMemorySize"]) / (1024 * 1024),2);
+
+                decimal memoriaLibre=
+                    Math.Round(Convert.ToDecimal(elemento["FreePhysicalMemory"])/(1024 * 1024),2);
+
+                salida.Text += Environment.NewLine +
+                    "Memoria Total: " + memoriaTotal+"GB";
+
+                salida.Text += Environment.NewLine +
+                    "Memoria Libre: " + memoriaLibre+"GB";
+
+                string arquitectura=elemento["OSArchitecture"].ToString();
+                string fabricante = elemento["Manufacturer"].ToString();
+                salida.Text += Environment.NewLine + arquitectura+fabricante; 
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
